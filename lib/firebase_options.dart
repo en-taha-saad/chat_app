ede3dde40bdc561d3b0359241584e68140980cba
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-28 15:09:50 +0300

    edits

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -20,21 +20,32 @@ class DefaultFirebaseOptions {
       return web;
     }
     // ignore: missing_enum_constant_in_switch
-    // switch (defaultTargetPlatform) {
-    //   case TargetPlatform.android:
-    //     return android;
-    //   case TargetPlatform.iOS:
-    //     return ios;
-    //   case TargetPlatform.macOS:
-    //     throw UnsupportedError(
-    //       'DefaultFirebaseOptions have not been configured for macos - '
-    //       'you can reconfigure this by running the FlutterFire CLI again.',
-    //     );
-    // }
-
-    throw UnsupportedError(
-      'DefaultFirebaseOptions are not supported for this platform.',
-    );
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.fuchsia:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for fuchsia - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+    }
   }
 
   static const FirebaseOptions web = FirebaseOptions(
